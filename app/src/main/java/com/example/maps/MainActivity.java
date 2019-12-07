ae6107f940ec052266014c0b2b7531d9c93f5d0a
Saim <saimh355@gmail.com>
2019-12-07 16:52:11 +0500

    Real time location

--- app/src/main/java/com/example/maps/MainActivity.java
+++ app/src/main/java/com/example/maps/MainActivity.java
@@ -1,13 +1,152 @@
 package com.example.maps;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.widget.Toast;
+
+import com.example.maps.Common.Common;
+import com.example.maps.Model.User;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference user_information;
+    private static final int MY_REQUEST_CODE = 7117;
+    List<AuthUI.IdpConfig> providers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Paper.init(this);
         setContentView(R.layout.activity_main);
+
+        user_information = FirebaseDatabase.getInstance().getReference(Common.USER_INFORMATION);
+
+        //Init Provider
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        //Request Permission
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                         showSignInOptions();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity.this, "you must Accept permissions to use app", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+    }
+
+    private void showSignInOptions() {
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),
+                MY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MY_REQUEST_CODE)
+        {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK)
+            {
+                final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                //Check If User exists or not
+                user_information.orderByKey()
+                        .equalTo(firebaseUser.getUid())
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.getValue() == null) //If user not exists in the database
+                                {
+                                        if (!dataSnapshot.child(firebaseUser.getUid()).exists()) {
+                                          Common.loggedUser = new User(firebaseUser.getUid(),firebaseUser.getEmail());
+                                          user_information.child(Common.loggedUser.getEmail())
+                                                .setValue(Common.loggedUser);
+                                        }
+                                        else
+                                        {
+                                            Common.loggedUser = dataSnapshot.child(firebaseUser.getUid()).getValue(User.class);
+                                        }
+                                        //Save UID to storage to update location from Background
+                                    Paper.book().write(Common.USER_UID_SAVE_KEY,Common.loggedUser.getUid());
+                                        updateToken(firebaseUser);
+                                        setupUI();
+
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+            }
+        }
+    }
+
+    private void setupUI() {
+          startActivity(new Intent(MainActivity.this,HomeActivity.class));
+          finish();
+    }
+
+    private void updateToken(final FirebaseUser firebaseUser) {
+        final DatabaseReference tokens = FirebaseDatabase.getInstance()
+                .getReference(Common.TOKENS);
+
+        //GET TOKEN
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+                    @Override
+                    public void onSuccess(InstanceIdResult instanceIdResult) {
+                          tokens.child(firebaseUser.getUid())
+                                  .setValue(instanceIdResult.getToken());
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
